43f64350eb6a66d5845a62e52ccbfc6da1f0a2c2
theRustyKnife <therustyknife13@gmail.com>
2017-07-06 11:05:50 +0200

    Added support for loading modules in the function format

--- FML/script/module-loader.lua
+++ FML/script/module-loader.lua
@@ -34,32 +34,52 @@ function _M.load_std(std, res_table, stage, config, version)
 end
 
 
-function _M.load_from_file(path, load_func, log_func)
+function _M.init(module, _M, ...)
+--[[
+Init a loaded module. If module is a function, it's going to be called with any other parameters passed. If _M wasn't
+passed, or it is nil, an empty table is going to be passed to the module instead.
+The resulting value is going to be returned, if the module returned a value and true, that value will be returned, plus
+true to indicate the fact that the module wishes not to use the table it was given.
+If module is anything else than a function, it's going to be returned as-is.
+]]
+	if type(module) == "function" then
+		local res = _M or {}
+		local ret, override = module(res, ...)
+		return (override and ret) or res, override
+	end
+	return module
+end
+
+function _M.load_from_file(path, load_func, init, log_func)
 --[[
 Load and return a module from the file at path, using load_func. Returns nil if the module didn't return a table.
 log_func can be nil, boolean or function(message). Logging won't work if load_func doesn't return the error as second
-return value.
+return value. If init is true, the module will be initialized using the init function and returned in it's final form,
+if it's a function, it will be called with the module as parameter and the return value of the function will be used as
+the final form.
 ]]
 	load_func = load_func or require
+	if init and type(init) ~= "function" then init = _M.init; end
 	if log_func == true then log_func = log; end
 	
 	local loaded, err = load_func(path)
-	if type(loaded) ~= "table" then
-		loaded = nil
+	if loaded == nil then
 		if err and log_func then
 			log_func("Loading FML module from '"..tostring(path).."' failed: "..(err or "No error message."))
 		end
+		return nil
 	end
 	
+	if init then return init(loaded); end
 	return loaded
 end
 
-function _M.load_from_files(modules, res_table, load_func, log_func)
+function _M.load_from_files(modules, res_table, load_func, init, log_func)
 --[[
 Load all the modules, calling load_from_file for each and putting them into res_table, indexed by their names.
 Since this only internal, we can just ignore any modules whose names are already in the table, assuming they're supposed
 to be there.
-load_func and log_func is passed to load_from_file.
+load_func, init and log_func is passed to load_from_file.
 ]]
 	res_table = res_table or {}
 	
