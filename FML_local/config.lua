84431d07b0c8f328a8b2222d471c9a30c3d78dc3
theRustyKnife <therustyknife13@gmail.com>
2017-07-06 11:37:16 +0200

    Added log config to local

--- FML_local/config.lua
+++ FML_local/config.lua
@@ -22,4 +22,16 @@ return {
 		{name = "GUI", path = ".modules.GUI"},
 		{name = "Semver", path = ".modules.Semver"},
 	},
+	
+	
+	LOG = {
+		-- If true, log messages will be printed to the console as well
+		IN_CONSOLE = true,
+		
+		-- If true, the respective level messsages will be logged. If false, the functions are replaced with empty ones,
+		-- so it's safe to leave the calls in, without much performance loss.
+		E = true,
+		W = true,
+		D = true,
+	},
 }
