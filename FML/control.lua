201ee405990319fc694a8d560e92e94507b45140
theRustyKnife <therustyknife13@gmail.com>
2017-07-16 18:32:33 +0200

    New hack for passing data
    This time it's using interface names, which should hopefully work well enough. Not tested in multiplayer...
    
    Nice thing: It's no longer required to restart the game for FML to reload.

--- FML/control.lua
+++ FML/control.lua
@@ -1,4 +1,32 @@
-local FML_import = loadstring(settings.global["FML_FML-hack-setting"].value)()
+-- Load FML for other mods
+local FML_stdlib = {}; require("script.FML-stdlib")(FML_stdlib)
+local module_loader = {}; FML_stdlib.safe_require("script.module-loader", true)(module_loader)
+
+local config = FML_stdlib.safe_require("config", true)
+
+
+local load_func = FML_stdlib.safe_require
+if config.FORCE_LOAD_MODULES then
+	load_func = function(path) return FML_stdlib.safe_require(path, config,FORCE_LOAD_MODULES); end
+end
+
+local to_export = {
+	config = config,
+	FML_stdlib = FML_stdlib.safe_require("script.FML-stdlib"),
+	module_loader = FML_stdlib.safe_require("script.module-loader"),
+	console = serpent.dump(FML_stdlib.safe_require("script.console")),
+	modules = {},
+}
+module_loader.load_from_files(config.MODULES_TO_LOAD, to_export.modules, load_func, false, log_func)
+local serialized = serpent.dump(to_export)
+
+-- Hacky interface data pass :|
+remote.add_interface("therustyknife.FML.serialized", {[serpent.dump(to_export)] = function()end})
+
+
+-- Load FML for the shared part
+local FML_import = next(remote.interfaces["therustyknife.FML.serialized"]); FML_import = loadstring(FML_import)()
+
 
 local module_loader = {}; FML_import.module_loader(module_loader)
 local FML_stdlib = module_loader.init(FML_import.FML_stdlib)
@@ -6,6 +34,7 @@ local config = FML_stdlib.safe_require(".config")
 
 
 local FML = module_loader.load_std(FML_stdlib, {TYPE = "shared"}, "runtime", config, config.VERSION)
+FML_stdlib.put_to_global("therustyknife", "FML", FML) -- Give global access to the library
 
 for _, module in ipairs(config.MODULES_TO_LOAD) do
 	if FML_import.modules[module.name] then
@@ -13,9 +42,8 @@ for _, module in ipairs(config.MODULES_TO_LOAD) do
 	end
 end
 
-
 --[[
 A function to allow for simple loading of FML. Intended for use in the console.
 Usage: /c loadstring(remote.call("therustyknife.FML.console", "get"))()
-]]
+--]]
 FML.remote.add_interface("therustyknife.FML.console", {get = function() return FML_import.console; end})
--- FML/script/console.lua
+++ FML/script/console.lua
@@ -1,5 +1,9 @@
+-- The function bellow will be serialized and run in the console environment when called.
+-- Usage: /c loadstring(remote.call("therustyknife.FML.console", "get"))()()
+-- Running that command will load FML into a global variable named FML. The statement also returns the FML instance if
+-- that's what you want.
 return function()
-	local FML_import = loadstring(settings.global["FML_FML-hack-setting"].value)()
+	local FML_import = next(remote.interfaces["therustyknife.FML.serialized"]); FML_import = loadstring(FML_import)()
 	local module_loader = {}; FML_import.module_loader(module_loader)
 	local FML_stdlib = module_loader.init(FML_import.FML_stdlib)
 	local config = FML_import.config
