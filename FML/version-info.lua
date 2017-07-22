4f56cd2eac0f9cc0455db4eeec9327dc270e5c9b
theRustyKnife <therustyknife13@gmail.com>
2017-07-22 13:33:13 +0200

    Increment version

--- FML/version-info.lua
+++ FML/version-info.lua
@@ -12,4 +12,10 @@ return {
 		name = "0.1.0-alpha.5.3",
 		changes = {"blueprint-data now handles entities on it's own"},
 	},
+	{
+		code = 11,
+		name = "0.1.0-alpha.6.0",
+		added = {"New functions in surface", "Object.typeof"},
+		changes = {"Various bugfixes"},
+	},
 }
