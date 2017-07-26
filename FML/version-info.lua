f03d7c376b4d5f950b9166fbd4a687a27413e8bc
theRustyKnife <therustyknife13@gmail.com>
2017-07-26 21:00:18 +0200

    Increment version

--- FML/version-info.lua
+++ FML/version-info.lua
@@ -18,4 +18,17 @@ return {
 		added = {"New functions in surface", "Object.typeof"},
 		changes = {"Various bugfixes"},
 	},
+	{
+		code = 12,
+		name = "0.1.0-alpha.6.1",
+		fixes = {"Object.typeof", "BlueprintData._copy", "blueprint-data items are now hidden"},
+		added = {"Some GUI functions, still not great...", "Object.abstract"},
+		changes = {"events no longer throws an error on attempt to register permanent handler"}
+	},
+	--[[
+	{
+		code = 13,
+		name = "",
+	},
+	--]]
 }
